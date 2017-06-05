35c367c177027a245055d97b92cfef46e8c6c6c7
diasaleh <dia-20111@hotmail.com>
2017-06-05 14:05:30 +0300

    ii

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,22 +2,45 @@
 
 @section('content')
 
-    <header>
-       <div class="jumbotron head" style = "background-image:linear-gradient(rgba(0, 0, 0,.5),rgba(0, 0, 0, .5)), url('head3.jpg')">
-         <div class="text-center logo"> 
-            <img class=" logo" src="vendor/img/logo6.png">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>First Slide</h3>
+                        <p>This is a description for the first slide.</p>
+                    </div>
+                </div>
+                <!-- Slide Two - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Second Slide</h3>
+                        <p>This is a description for the second slide.</p>
+                    </div>
+                </div>
+                <!-- Slide Three - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Third Slide</h3>
+                        <p>This is a description for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-
-            <h1 class="display-3 title">Make a Difference Today</h1>
-             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-            <p></p>
-            <p class="lead">
-                <a class="btn btn-primary btn-lg bv" href="{{ route('register') }}" role="button">JOIN US</a>
-                <a class="btn btn-primary btn-lg donate-btn" href="#" role="button">DONATE NOW</a>
-            </p>
-            
-        </div>
-    </header>
+       
 
     <!-- Page Content -->
     <div class="container-fluid">
