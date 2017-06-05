ad1d60f0f970b02cf4d932982a8db455f9d211c1
diasaleh <dia-20111@hotmail.com>
2017-06-05 12:21:12 +0300

    j

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -36,16 +36,16 @@
 Sign in <span class="sr-only"></span></a>
  @else
                             <li class="dropdown nav-item">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
-                                        <a href="{{ route('logout') }}"
+                                        <a class="dropdown-item"  href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logoutda
+                                            Logout
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
