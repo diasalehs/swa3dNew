b5e7bc667a9c265f3bc651b60407cdcdb519471f
diasaleh <dia-20111@hotmail.com>
2017-06-08 14:38:12 +0300

    edit footer hover

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -29,7 +29,7 @@
                     </ul>
                 </div>
                 <div class="col-sm-3">
-                    <div class="social-networks">
+                    <div class="social-networks" id="w">
                         <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
                         <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
                         <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
