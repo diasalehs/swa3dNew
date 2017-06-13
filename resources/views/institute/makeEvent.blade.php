8f446cf81131af5151d29b0bdd830c66e5709915
diasaleh <dia-20111@hotmail.com>
2017-06-13 15:10:34 +0300

    style make event

--- resources/views/institute/makeEvent.blade.php
+++ resources/views/institute/makeEvent.blade.php
@@ -5,7 +5,6 @@
     <div class="row">
       @include('institute/includes.sidebar')
         <div class="col-sm-12  col-md-8  col-lg-9" style="color: #333">
-             <div class="col-lg-8 offset-md-2">
               <div class="card">
                   <div class="card-header">Create Event</div>
                   <div class="card-block">
@@ -13,80 +12,67 @@
                       <form class="" role="form" method="POST" action="{{route('event') }}">{{ csrf_field() }}
 
                           <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                              <label for="name" class="col-lg-4 form-control-label">Title</label>
-                              <div class="col-lg-6">
-                                  <input id="name" type="text" class="form-control" name="title" value="{{ old('title') }}"
+                              <label for="name" class="form-control-label">Title</label>
+                              <input id="name" type="text" class="form-control" name="title" value="{{ old('title') }}"
                                   required="required" />
                                   @if ($errors->has('title'))
                                       <div class="alert alert-danger" role="alert">
                                           <strong>Warning!</strong> {{ $errors->first('title') }}
                                       </div>
                                   @endif
-                              </div>
                           </div>
 
-                          <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
-                              <div class="col-lg-6">
+                          <div class="form-group {{ $errors->has('description') ? ' has-error' : '' }}">
                                   <div class="form-group"> <!-- Description field -->
-                                    <label class="control-label " for="description">description</label>
-                                    <textarea value="{{ old('description') }}" required="required" name="description" id="description"></textarea>
+                                    <label class="control-label " for="description">Description</label>
+                                    <textarea class="form-control" value="{{ old('description') }}" required="required" name="description" id="description"></textarea>
                                   </div>
                                   @if ($errors->has('description'))
                                       <div class="alert alert-danger" role="alert">
                                           <strong>Warning!</strong> {{ $errors->first('description') }}
                                       </div>
                                   @endif
-                              </div>
                           </div>
 
                           <div class="form-group{{ $errors->has('cover') ? ' has-error' : '' }}">
                               <label for="name" class="col-lg-4 form-control-label">uplode cover</label>
-                              <div class="col-lg-6">
                                   <input id="name" type="file" accept="image/*" class="form-control" name="cover" />
                                   @if ($errors->has('cover'))
                                       <div class="alert alert-danger" role="alert">
                                           <strong>Warning!</strong> {{ $errors->first('cover') }}
                                       </div>
                                   @endif
-                              </div>
                           </div>
 
                           <div class="form-group{{ $errors->has('startDate') ? ' has-error' : '' }}">
-                              <label for="name" class="col-lg-4 form-control-label">Start Date</label>
-                              <div class="col-lg-6">
-                                  <input id="name" type="date" class="form-control" name="startDate" value="2018-08-19"
+                              <label for="name" class="form-control-label">Start Date</label>
+                              <input id="name" type="date" class="form-control" name="startDate" min = "<?php echo date("Y-m-d"); ?>" value="<?php echo date("Y-m-d"); ?>"
                                   required="required" />
                                   @if ($errors->has('startDate'))
                                       <div class="alert alert-danger" role="alert">
                                           <strong>Warning!</strong> {{ $errors->first('startDate') }}
                                       </div>
                                   @endif
-                              </div>
                           </div>
 
                           <div class="form-group{{ $errors->has('endDate') ? ' has-error' : '' }}">
-                              <label for="name" class="col-lg-4 form-control-label">End Date</label>
-                              <div class="col-lg-6">
-                                  <input id="name" type="date" class="form-control" name="endDate" value="2018-08-20"
+                              <label for="name" class=" form-control-label">End Date</label>
+                                  <input id="name" type="date" class="form-control" name="endDate" min="<?php echo date("Y-m-d"); ?>" value="<?php echo date("Y-m-d"); ?>"
                                   required="required" />
                                   @if ($errors->has('endDate'))
                                       <div class="alert alert-danger" role="alert">
                                           <strong>Warning!</strong> {{ $errors->first('endDate') }}
                                       </div>
                                   @endif
-                              </div>
                           </div>
 
                           <div class="form-group">
-                              <div class="col-lg-4 offset-md-4">
-                                  <button type="submit" class="btn btn-success btn-block">Create</button>
-                              </div>
+                                  <button type="submit" class="btn btn-success btn-green btn-block">Create Event</button>
                           </div>
 
                       </form>
                   </div>
               </div>
-          </div>
         </div>
     </div>
 </div>
