0c719b2650331a4f1554764a0b15f659e4903e2d
diasaleh <dia-20111@hotmail.com>
2017-06-13 12:09:40 +0300

    style all news

--- resources/views/allNews.blade.php
+++ resources/views/allNews.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
 
     <!-- Page Heading/Breadcrumbs -->
-    <h1 class="mt-4 mb-3">Blog Home Two <small>Subheading</small></h1>
+    <h1 class="mt-4 mb-3">All News <small></small></h1>
 
     <ol class="breadcrumb">
         <li class="breadcrumb-item">
-            <a href="index.html">Home</a>
+            <a href="{{ route('main') }}">Home</a>
         </li>
-        <li class="breadcrumb-item active">Blog Home 2</li>
+        <li class="breadcrumb-item active">All News</li>
     </ol>
 
     <!-- Blog Post -->
@@ -19,87 +19,25 @@
             <div class="row">
                 <div class="col-lg-6">
                     <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
+                        <img class="img-fluid rounded all-news-img" src="http://placehold.it/750x300" alt="">
                     </a>
                 </div>
                 <div class="col-lg-6">
                     <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisici</p>
+                    <a href="#" class="btn btn-primary btn-green">Read More &rarr;</a>
                 </div>
             </div>
         </div>
         <div class="card-footer text-muted">
-            Posted on January 1, 2017 by <a href="#">Start Bootstrap</a>
+            Posted on January 1, 2017
         </div>
     </div>
 
-    <!-- Blog Post -->
-    <div class="card mb-4">
-        <div class="card-block">
-            <div class="row">
-                <div class="col-lg-6">
-                    <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
-                    </a>
-                </div>
-                <div class="col-lg-6">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer text-muted">
-            Posted on January 1, 2017 by <a href="#">Start Bootstrap</a>
-        </div>
-    </div>
-
-    <!-- Blog Post -->
-    <div class="card mb-4">
-        <div class="card-block">
-            <div class="row">
-                <div class="col-lg-6">
-                    <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
-                    </a>
-                </div>
-                <div class="col-lg-6">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer text-muted">
-            Posted on January 1, 2017 by <a href="#">Start Bootstrap</a>
-        </div>
-    </div>
-
-    <!-- Blog Post -->
-    <div class="card mb-4">
-        <div class="card-block">
-            <div class="row">
-                <div class="col-lg-6">
-                    <a href="#">
-                        <img class="img-fluid rounded" src="http://placehold.it/750x300" alt="">
-                    </a>
-                </div>
-                <div class="col-lg-6">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer text-muted">
-            Posted on January 1, 2017 by <a href="#">Start Bootstrap</a>
-        </div>
-    </div>
 
     <!-- Pagination -->
     <nav aria-label="Page navigation example">
-      <ul class="pagination">
+      <ul class="pagination justify-content-center">
         <li class="page-item">
           <a class="page-link" href="#" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
