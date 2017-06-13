7aa485d6b3f2d70625bf1d904d7232f38d9c9ccc
laithabukhader <khaderlaith@gmail.com>
2017-06-13 13:23:45 +0300

    allNews

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -1,52 +1,53 @@
-    <?php
-    namespace App\Http\Controllers;
+<?php
+namespace App\Http\Controllers;
 
-    use Illuminate\Support\Facades\DB;
-    use App\Http\Controllers\Controller;
-    use Illuminate\Http\Request;
-    use App\User;
-    use App\Individuals;
-    use Illuminate\Support\Facades\auth;
-    use Illuminate\Database\Query\Builder;
-    use App\Http\Requests;
-    use App\news;
-    use Image;
-    class adminController extends Controller
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use App\Individuals;
+use Illuminate\Support\Facades\auth;
+use Illuminate\Database\Query\Builder;
+use App\Http\Requests;
+use App\news;
+use Image;
+class adminController extends Controller
+{
+    public function delete($userId)
     {
-        public function delete($userId)
-        {
-            $user = User::find($userId);
-            if($user->userType== 0){        $user->Individuals()->delete();}
-            if($user->userType== 1){        $user->Institute()->delete(); }
-            if($user->userType== 2){        $user->Researcher()->delete(); }
+        $user = User::find($userId);
+        if($user->userType== 0){        $user->Individuals()->delete();}
+        if($user->userType== 1){        $user->Institute()->delete(); }
+        if($user->userType== 2){        $user->Researcher()->delete(); }
 
-            $user->delete();
-            return redirect()->route('home');
+        $user->delete();
+         return redirect()->route('home');
 
-        	# code...
-        }
-        public function index()
-        {
-            if(Auth::attempt() || Auth::user()){
-            	$user = Auth::user();
-            	if ($user->userType=== 10 ) {
-            		return view('admin/adminDashboard',["users_record"=>User::paginate(10)]);
-            	}
-            	else{
-                    return redirect()->route('home');
-                }
-            }else{
-                return redirect()->route('main');
+    	# code...
+    }
+ public function index()
+    {
+        if(Auth::attempt() || Auth::user()){
+        	$user = Auth::user();
+        	$users_record= DB::table('users')->get();
+        	if ($user->userType=== 10 ) {
+        		return view('admin/adminDashboard',["users_record"=>User::paginate(10)]);
+        	}
+        	else{
+                return redirect()->route('home');
             }
+        }else{
+                return redirect()->route('main');
         }
+    }
 
 
-        public function indexx()
-        {   $news_record= DB::table('news')->get();
-        return view('admin/adminNews',["news_record"=>$news_record]);
+    public function indexx()
+    {   $news_record= DB::table('news')->get();
+         return view('admin/adminNews',["news_record"=>$news_record]);
 
 
-            # code...
+        # code...
     }
     public function edit($newsID)
 
@@ -54,12 +55,12 @@
         $news = news::find($newsID);
         return view('admin/editingpage',["news"=>$news]);
     }
-    public function adminNewsView()
+     public function adminNewsView()
     {    
         $news_record= DB::table('news')->get();
-        return view('admin/adminNewsView',["news_record"=>$news_record]);
+         return view('admin/adminNewsView',["news_record"=>$news_record]);
 
 
-            # code...
+        # code...
     }
 }
