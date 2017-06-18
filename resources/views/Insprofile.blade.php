60364ac4fc2c2fb8ef4d48c31982c7d1f54c586d
laithabukhader <khaderlaith@gmail.com>
2017-06-18 15:13:57 +0300

    rating all done

--- resources/views/Insprofile.blade.php
+++ resources/views/Insprofile.blade.php
@@ -6,11 +6,11 @@
   <div class="jumbotron jumbotron-fluid text-center">
     <div class="container">
       <div class=" circular--landscape">
-          <img class="profile-pic text-center" src="{{ URL::to('/') }}/pp/{{$Individual[0]->picture}}">
+          <img class="profile-pic text-center" src="{{ URL::to('/') }}/pp/{{$Institute[0]->picture}}">
       </div>
 
-      <h1 class="display-7">{{$Individual[0]->nameInEnglish}}</h1>
-      <p class=""><span>Volunteer</span> <span>{{$Individual[0]->country}}</span> <span>{{$Individual[0]->cityName}}</span> </p>
+      <h1 class="display-7">{{$Institute[0]->nameInEnglish}}</h1>
+      <p class=""><span>Institute</span> <span>{{$Institute[0]->country}}</span> <span>{{$Institute[0]->cityName}}</span> </p>
 
       @if(Auth::guest())
              <a class='btn btn-green'  href="{{route('login')}}">follow</a>
@@ -35,11 +35,13 @@
                     <h4 class="card-title">User Informations</h4>
                     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                     <p class="card-text">
-                    Gender: {{$Individual[0]->gender}}<br>
-                    Birth Date: {{$Individual[0]->dateOfBirth}}<br>
-                    Education: {{$Individual[0]->educationalLevel}}<br>
-                    Current Work: {{$Individual[0]->currentWork}}<br>
-                    Availabel from: {{$Individual[0]->availableFrom}} to: {{$Individual[0]->availableTo}}<br>
+                    address: {{$Institute[0]->address}}<br>
+                    Establishment year: {{$Institute[0]->establishmentYear}}<br>
+                    Employment rate: {{$Institute[0]->employmentRate}}<br>
+                    Number of employees: {{$Institute[0]->numOfEmployees}}<br>
+                    Number of stakeholders: {{$Institute[0]->numOfStakeholders}} <br>
+                    Work summary: {{$Institute[0]->workSummary}} <br>
+                    license: {{$Institute[0]->license}} <br>
                     <a href="#" class="card-link">Card link</a>
                     <a href="#" class="card-link">Another link</a>
                   </div>
@@ -50,9 +52,13 @@
                     <h4 class="card-title">Contact Informations</h4>
                     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                     <p class="card-text">
-                    Email: {{$Individual[0]->email}}<br>
-                    Adress: {{$Individual[0]->address}}<br>
-                    Mobile number: {{$Individual[0]->mobileNumber}}<br>
+                    Email: {{$Institute[0]->email}}<br>
+                    Adress: {{$Institute[0]->address}}<br>
+                    Mobile number: {{$Institute[0]->mobileNumber}}<br>
+                    PObox: {{$Institute[0]->PObox}}<br>
+                    Fax: {{$Institute[0]->fax}}<br>
+                    Website: <a href="{{$Institute[0]->website}}">{{$Institute[0]->website}}</a><br>
+                    Facebook Page: <a href="{{$Institute[0]->facebookPage}}">{{$Institute[0]->facebookPage}}</a><br>
                     <a href="#" class="card-link">Card link</a>
                     <a href="#" class="card-link">Another link</a>
                   </div>
@@ -61,7 +67,7 @@
 
 
           </div>
-          <div class="col-4">
+           <div class="col-4">
             <div class="card">
               <div class="card-header">
                 Dashboard
@@ -72,16 +78,40 @@
                   <tbody>
                     <tr>
                       <td>Voluntary years</td>
-                      <td>{{$Individual[0]->voluntaryYears}}</td>
+                      {{-- <td>{{$Institute[0]->voluntaryYears}}</td> --}}
                     </tr>
                     <tr>
-                      <td>Otto</td>
-                      <td>@mdo</td>
+                      <td>cat1</td>
+                      <td>{{$Institute[0]->cat1}}</td>
                     </tr>
                     <tr>
-                      <td>Otto</td>
-                      <td>@mdo</td>
+                      <td>cat2</td>
+                      <td>{{$Institute[0]->cat2}}</td>
                     </tr>
+                     <tr>
+                      <td>cat3</td>
+                      <td>{{$Institute[0]->cat3}}</td>
+                    </tr>
+                     <tr>
+                      <td>cat4</td>
+                      <td>{{$Institute[0]->cat4}}</td>
+                    </tr>
+                     <tr>
+                      <td>acc </td>
+                      <td>{{$Institute[0]->acc_avg}}</td>
+                    </tr>
+                    <tr>
+                      <td> </td>
+                      <td>
+                      {{-- @if(auth::check()) --}}
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                       Rate!
+                      </button>
+                      {{-- @endif --}}
+                    </td>
+                    </tr>
+
+                     
 
 
                   </tbody>
@@ -91,6 +121,39 @@
           </div>
         </div>
       </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Rate</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="{{route('rank',$Institute[0]->id)}}" method="get">
+      <div class="modal-body">
+        <label>cat1</label>
+        <input type="text" name="cat1"><br>
+        <label>cat2</label>
+        <input type="text" name="cat2"><br>
+        <label>cat3</label>
+        <input type="text" name="cat3"><br>
+        <label>cat4</label>
+        <input type="text" name="cat4"><br>
+          
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit"  class="btn btn-primary">Save</button>
+
+      </div>
+      </form>
+
+    </div>
+  </div>
 </div>
 
 
