fe94b93787b0a6d9c810a6cd245cb8f4db440a8a
diasaleh <dia-20111@hotmail.com>
2017-06-19 11:31:04 +0300

    j

--- resources/views/institute/following.blade.php
+++ resources/views/institute/following.blade.php
@@ -11,30 +11,30 @@ use App\user;
 
 	         <div class="col-sm-12  col-md-8  col-lg-9" style="color: #333">
           <h1>Following</h1>
+          <div class="row  justify-content-between">
 
-          <table class="table">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-              <?php
-              foreach ($following as $followi) {
-                $usere=User::findOrFail($followi->requested_id);
-                echo "<tr>
-                <td>".$usere->name."</td>
-                <td>".$usere->email."</td>
-                <td>
-                <a class='btn btn-success btn-block'  href='allusers/unfollow/".$usere->id."'>unfollow</a>
-                </td>
-                </tr>";
-              }
-              ?>
-              </tbody>
-          </table>
+          @foreach ($following as $followi)
+            <?php $usere=User::findOrFail($followi->requested_id);?>
+          <div class="card col-5 " style="margin: 2px; padding:5px">
+              <div class="card-block" style="padding:5px;">
+                  <div class="row">
+                      <div class="col-lg-6">
+                          <a href="#">
+                            <img class="img-fluid rounded all-news-img" src="{{ URL::to('/') }}/pp/{{$user->picture}}" alt="">
+                        </a>
+
+                    </div>
+                    <div class="col-lg-6">
+                      <h5 class="card-title">{{$usere->name}}</h5>
+                      <p class="card-text line-clamp">{{$usere->email}}</p>
+                      <a href="allusers/unfollow/'.<?php $usere->id ?>.'" class="btn btn-danger">UnFollow</a>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+      @endforeach
+    </div>
 
          </div>
     </div>
