bf62915e637a48f286742ea806fcbeaa7ca59c86
diasaleh <dia-20111@hotmail.com>
2017-07-09 17:18:06 +0300

    reset style

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,36 +3,41 @@
 @section('content')
 <div class="container" style="margin: 120px auto">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
+        <div class="col-lg-6 offset-md-3">
+            <div class="card">
+                <div class="card-header">Reset Password</div>
+                <div class="card-block">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
+                    @if ($errors->has('email'))
 
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                          <strong>Error! </strong> {{ $errors->first('email') }}
+                        </div>
+
+                    @endif
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-12 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12 ">
+                                <button type="submit" class="btn btn-green btn-block">
                                     Send Password Reset Link
                                 </button>
                             </div>
--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -3,115 +3,209 @@
 @section('content')
 
 @if($event->endDate > $date)
+<div class="viewProfile" style="margin-bottom:30px;">
 
 @if(Auth::guest())
-<div class="container-fluid" style="margin:30px auto; padding:5px;">
+
+  <div class="jumbotron jumbotron-fluid text-center"style="background-image: linear-gradient(rgba(19, 58, 83, 0.8),rgba(19, 58, 83, 0.8)),url({{URL::to('/')}}/events/{{$event->cover}});
+  background-size:contain;background-repeat: no-repeat;
+  background-position: right top;
+  background-attachment: fixed;
+  ">
+    <div class="container">
+      <h1 class="display-7 " style="color:#fff">{{$event->title}}</h1>
+      <p class=""style="color:#fff">{{$event->description}}</p>
+      <p class=""style="color:#fff; margin-bottom:20px">{{$event->startDate}} To {{$event->endDate}} - in Nablus</p>
+      <a href="{{route('login')}}" class="btn btn-pink ">Volunteer</a>
+      <a href="{{route('login')}}" class="btn btn-yellow ">Follow</a>
+    </div>
+  </div>
+  <div class="container">
     <div class="row">
-         <div class="col-12" style="color: #333">
-          <div class="row justify-content-center">
-          <div class="col-md-8 col-sm-12">
-              <div class="card card-inverse event">
-                <img class="card-img" src="{{URL::to('/')}}/events/{{$event->cover}}" alt="Card image">
-                <div class="card-img-overlay">
-                  <h3 class="card-title">{{$event->title}}</h3>
-                  <p class="card-text line-clamp-4">{{$event->description}}</p>
-                  <p class="">{{$event->startDate}} To {{$event->endDate}} - in Nablus</p>
-                  <a href="{{route('login')}}" class="card-link pink-link">Volunteer</a>
-                  <a href="{{route('login')}}" class="card-link yellow-link ">Follow</a>
-                </div>
+
+        <div class="col-lg-8">
+          <h3 class="greencolor ">Event Posts</h3>
+          <hr />
+          @if($event->open)
+            <div class="card" style="margin-bottom:20px;">
+              <div class="card-block">
+                <h4 class="card-title greencolor" >Post Title</h4>
+                <p class="card-text">
+                  It is a long established
+                  fact that a reader will be distracted
+                   by the readable
+                   content of a page when looking at its layout.
               </div>
             </div>
+            <div class="card"style="margin-bottom:20px;">
+              <div class="card-block">
+                <h4 class="card-title greencolor">Post Title</h4>
+                <p class="card-text" style="color: var(--navy);">
+                  It is a long established
+                  fact that a reader will be distracted
+                   by the readable
+                   content of a page when looking at its layout.
               </div>
+            </div>
+          @endif
 
-         </div>
 
-    </div>
-</div>
+          </div>
+          <div class=" col-lg-4">
+
+            <h3 class="greencolor ">Volunteers in this event</h3>
+            <hr />
+
+            <div class="list-group" style="color: var(--navy);">
+              <button type="button" class="list-group-item list-group-item-action">
+                Cras justo odio
+              </button>
+              <button type="button" class="list-group-item list-group-item-action" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Dapibus ac facilisis in</button>
+              <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
+              <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
+              <button type="button" class="list-group-item list-group-item-action" >Vestibulum at eros</button>
+              <button type="button" class="list-group-item list-group-item-action btn-green seeall" >See All</button>
+            </div>
+
+            <div>
+
+            </div>
+
+
+          </div>
+        </div>
+      </div>
 @endif
 
 @if(Auth::check())
-<div class="container-fluid" style="margin:30px auto; padding:5px;">
-    <div class="row">
-         <div class="col-12" style="color: #333">
-          <div class="row justify-content-center">
-          <div class="col-md-8 col-sm-12">
-              <div class="card card-inverse event">
-                <img class="card-img" src="{{URL::to('/')}}/events/{{$event->cover}}" alt="Card image">
-                <div class="card-img-overlay">
-                  <h3 class="card-title">{{$event->title}}</h3>
-                  <p class="card-text line-clamp-4">{{$event->description}}</p>
-                  <p class="">{{$event->startDate}} To {{$event->endDate}} - in {{$event->city}}</p>
-                  @if($user->userType == 0)
-                      @if($request)
-                        <a href="{{route('disVolunteer',[$event->id])}}" class="card-link pink-link">Cancel Volunteer Request</a>
-                      @elseif(!$request)
-                        <a href="{{route('volunteer',[$event->id])}}" class="card-link pink-link">Volunteer Request</a>
-                      @endif
-                      <a href='event/{{$event->id}}' class="card-link yellow-link ">Follow</a>
-                  @elseif($mine && $archived == 0)
-                    <a class="card-link pink-link" href="{{route('eventDelete',[$event->id])}}">Delete</a>
-                    <a class="card-link yellow-link" href="{{route('eventVeiwEdit',[$event->id])}}">Edit</a>
-                  @endif
-                </div>
-              </div>
-            </div>
-              </div>
-         </div>
-              <br>
-    </div>
-<br><br>
 
+  <div class="jumbotron jumbotron-fluid text-center"style="background-image: linear-gradient(rgba(19, 58, 83, 0.8),rgba(19, 58, 83, 0.8)),url({{URL::to('/')}}/events/{{$event->cover}});
+  background-size:contain;background-repeat: no-repeat;
+  background-position: right top;
+  background-attachment: fixed;
+  ">
+    <div class="container">
+      <h1 class="display-7 " style="color:#fff">{{$event->title}}</h1>
+      <p class=""style="color:#fff">{{$event->description}}</p>
+      <p class=""style="color:#fff; margin-bottom:20px">{{$event->startDate}} To {{$event->endDate}} - in Nablus</p>
+      @if($user->userType == 0)
+          @if($request)
+            <a href="{{route('disVolunteer',[$event->id])}}" class="btn btn-pink">Cancel Volunteer Request</a>
+          @elseif(!$request)
+            <a href="{{route('volunteer',[$event->id])}}" class="btn btn-pink">Volunteer Request</a>
+          @endif
+          <a href='event/{{$event->id}}' class="btn btn-yellow ">Follow</a>
+      @elseif($mine && $archived == 0)
+        <a class="btn btn-pink" href="{{route('eventDelete',[$event->id])}}">Delete</a>
+        <a class="btn btn-yellow" href="">Edit</a>
+      @endif
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+
+        <div class="col-lg-8">
         @if($mine)
 
         @if($archived == 0 || $archived == 2)
-        <form class="" role="form" method="POST" action="{{ route('post') }}">{{ csrf_field() }}
-          <div class="">
-            <input id="name" type="text" style="display: none;" class="form-control" name="event_id" value="{{ $event->id }}"/>
+        <div id="accordion" role="tablist" aria-multiselectable="true">
+      <div class="card">
+        <div class="card-header" role="tab" id="headingOne">
+          <h5 class="mb-0">
+            <a data-toggle="collapse" class="green-link" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Advance Search
+            </a>
+          </h5>
+        </div>
+
+        <div id="collapseOne" class="collapse " role="tabpanel" aria-labelledby="headingOne">
+          <div class="card-block"class="">
+            <form class="" role="form" method="POST" action="{{ route('post') }}">{{ csrf_field() }}
+              <div class="">
+                <input id="name" type="text" style="display: none;" class="form-control" name="event_id" value="{{ $event->id }}"/>
+              </div>
+                <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
+                    <label for="title" class="col-lg-4 form-control-label">make a post</label>
+                    <div class="col-lg-6">
+                        <textarea class="form-control" required="required" name="body" id="body">{{ old('body') }}</textarea>
+                        @if ($errors->has('body'))
+                            <div class="alert alert-danger" role="alert">
+                                <strong>Warning!</strong> {{ $errors->first('body') }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-lg-2">
+                        <button type="submit" class="btn btn-green btn-block">post</button>
+                    </div>
+                </div>
+              </form>
+
           </div>
-            <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
-                <label for="title" class="col-lg-4 form-control-label">make a post</label>
-                <div class="col-lg-6">
-                    <textarea class="form-control" required="required" name="body" id="body">{{ old('body') }}</textarea>
-                    @if ($errors->has('body'))
-                        <div class="alert alert-danger" role="alert">
-                            <strong>Warning!</strong> {{ $errors->first('body') }}
-                        </div>
-                    @endif
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-lg-2">
-                    <button type="submit" class="btn btn-green btn-block">post</button>
-                </div>
-            </div>
-          </form>
+        </div>
+    </div>
+    </div>
+
           @endif
+
+
+
+                  <h3 class="greencolor ">Event Posts</h3>
+                  <hr />
+                  @if($event->open)
+                  @foreach($posts as $post)
+                    <div class="card" style="margin-bottom:20px;">
+                      <div class="card-block">
+                        <h4 class="card-title greencolor" >Post Title</h4>
+                        <p class="card-text">
+                          {{$post->body}}
+                      </div>
+                    </div>
+                    @endforeach
+                  @endif
+
+
+                  </div>
+                  <div class=" col-lg-4">
+
+                    <h3 class="greencolor ">Volunteers in this event</h3>
+                    <hr />
+                    <table class="table table-responsive text-center  table-fixed">
+                    <thead class="text-center">
+                      <tr>
+                        <th>Name</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach($eventVols as $eventVol)
+                        <tr>
+                          <td>
+                          <a class='btn'  href="{{route('profile',[$eventVol->user_id])}}">{{$eventVol->nameInEnglish}}</a>
+                          </td>
+                          <td>
+                          <a class="btn btn-primary btn-sm" href="{{route('acceptVolunteer',['volunteerId'=>$eventVol->id , 'eventId' => $event->id])}}">Accept</a>
+                          </td>
+                        </tr>
+                      @endforeach
+
+
+                    </tbody>
+                  </table>
+
+
+                  </div>
+                </div>
+              </div>
+
+
 
           <div class="col-lg-12">
             <hr>
 
-            <table class="table table-responsive text-center  table-fixed">
-            <thead class="text-center">
-              <tr>
-                <th>Name</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach($eventVols as $eventVol)
-                <tr>
-                  <td>
-                  <a class='btn'  href="{{route('profile',[$eventVol->user_id])}}">{{$eventVol->nameInEnglish}}</a>
-                  </td>
-                  <td>
-                  <a class="btn btn-primary btn-sm" href="{{route('acceptVolunteer',['volunteerId'=>$eventVol->id , 'eventId' => $event->id])}}">Accept</a>
-                  </td>
-                </tr>
-              @endforeach
 
-
-            </tbody>
-          </table>
           </div>
         @endif
 
