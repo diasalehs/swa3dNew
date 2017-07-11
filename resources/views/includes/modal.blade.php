6340150d4098db3a09955da7dc0bcd22469e42ad
diasaleh <dia-20111@hotmail.com>
2017-07-11 14:20:30 +0300

    modal include

---
+++ resources/views/includes/modal.blade.php
@@ -0,0 +1,41 @@
+<!-- Modal -->
+        <div class="modal fade" id="invite" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Your Upcoming Events</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="row justify-content-center">
+
+                  <form id="invite" role="form" action="{{route('invite')}}" method="POST" style="display: flex; justify-content:center; flex-wrap: wrap; align-items: flex-start;"> {{ csrf_field() }}
+
+                    <div class="form-group"style="margin-left:15px;">
+                     <select name="invited[]"class="form-control" id="Select1" multiple>
+                      @foreach($userUevents as $userUevent)
+                        <option value="{{$userUevent->id}}">{{$userUevent->title}}</option>
+                      @endforeach
+                     </select>
+                   </div>
+
+                    <input id="userId" type="text" style="display: none;" class="form-control" name="userId" value="{{ $Individual->user->id }}"/>
+
+                        <div class="form-group">
+                          <button type="submit" class="btn btn-block btn-green">Invite</button>
+                        </div>
+
+                    </form>
+
+
+
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
