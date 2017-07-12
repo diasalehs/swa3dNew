8e493ed3f5469afe921ce8b9dbed1ca890ac1b78
diasaleh <dia-20111@hotmail.com>
2017-07-12 17:14:23 +0300

    all re

--- resources/views/allResearches.blade.php
+++ resources/views/allResearches.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.master')
 
 @section('content')
-<form method="get" action="{{route('Researches_search')}}"><input type="text" name="search">
-<button type="submit">search</button></form>
+<div class="container" style="margin-top:40px; min-height:600px;" >
 
+    <!-- Page Heading/Breadcrumbs -->
+    <h1 class="mt-4 mb-3 pinkcolor">All Researches <small></small></h1>
 
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a class="pink-link" href="{{ route('main') }}">Home</a>
+        </li>
+        <li class="breadcrumb-item active">All Researches</li>
+    </ol>
+
+    <form method="get" action="{{route('Researches_search')}}" role="form" id="form-buscar">
+      <div class="form-group">
+        <div class="input-group">
+          <input id="1" class="form-control" type="text" name="search" placeholder="Search..." required/>
+          <span class="input-group-btn">
+            <button class="btn btn-pink" style="  outline: 0 !important;
+ border-top-left-radius:0px!important;border-bottom-left-radius:0px!important;" type="submit">
+            <i class="glyphicon glyphicon-search" aria-hidden="true"></i> Search
+            </button>
+          </span>
+        </div>
+      </div>
+    </form>
 
 <div class="row">
              @foreach($researches as $research)
@@ -19,7 +40,9 @@
                 </div>
             </div>
             @endforeach
+          </div>
+
+{{ $researches->links('vendor.pagination.custom')}}
 </div>
 
-{{ $researches->links()}}
 @endsection('content')
