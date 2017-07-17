a205b15a2334242eddf9219fbd207e08ac253b74
diasaleh <dia-20111@hotmail.com>
2017-07-17 14:18:54 +0300

    fix review modal

--- resources/views/includes/reviewModal.blade.php
+++ resources/views/includes/reviewModal.blade.php
@@ -1,41 +1,42 @@
 <!-- Modal -->
 @if(Auth::check())
-        <div class="modal fade" id="invite" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="lessonsModal"id="invite" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="lessonsModal">Your Upcoming Events</h5>
+                <h5 class="modal-title" >Your Upcoming Events</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
               <div class="modal-body">
-                <div class="row justify-content-center">
 
-                  <form id="invite" role="form" action="{{route('invite')}}" method="POST" style="display: flex; justify-content:center; flex-wrap: wrap; align-items: flex-start;"> {{ csrf_field() }}
+                  <form id="invite" role="form" action="{{route('invite')}}" method="POST"> {{ csrf_field() }}
+                    <div class="row justify-content-center">
 
-                    <div class="form-group has-success">
+                    <div class="form-group has-success col-12">
                       <label for="exampleTextarea">Positive Feedback</label>
                       <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
                     </div>
-                    <div class="form-group has-danger">
+                    <div class="form-group has-danger col-12">
                       <label for="exampleTextarea">Negative Feedback</label>
                       <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
                     </div>
 
-                        <div class="form-group">
-                          <button type="submit" class="btn btn-block btn-green">Send</button>
-                        </div>
 
-                    </form>
+                      </div>
 
 
 
-                </div>
+
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn  btn-green">Send</button>
+
               </div>
+            </form>
+
             </div>
           </div>
         </div>
