a839b427ae0bd6395c074f77120f49e8ea1edc69
diasaleh <dia-20111@hotmail.com>
2017-07-25 16:01:47 +0300

    RRT

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -159,6 +159,55 @@
     <script type="text/javascript">
 
     $(document).ready(function() {
+       var table = $('#unacceptedT').DataTable({
+          'columnDefs': [
+             {
+                'targets': 0,
+                'render': function(data, type, row, meta){
+                       if(type === 'display'){
+                          data = '<div class="checkbox"><input type="checkbox" class="dt-checkboxes"><label></label></div>';
+                       }
+
+                       return data;
+                    },
+                'checkboxes': {
+                   'selectRow': true,
+                   'selectAllRender': '<div class="checkbox"><input type="checkbox" class="dt-checkboxes"><label></label></div>'
+
+                }
+             }
+          ],
+          'select': {
+             'style': 'multi'
+          },
+          'order': [[1, 'asc']]
+       });
+
+       // Handle form submission event
+       $('#frm-unaccepted').on('submit', function(e){
+          var form = this;
+
+          var rows_selected = table.column(0).checkboxes.selected();
+
+          // Iterate over all selected checkboxes
+          $.each(rows_selected, function(index, rowId){
+             // Create a hidden element
+             $(form).append(
+                 $('<input>')
+                    .attr('type', 'hidden')
+                    .attr('name', 'unaccepted[]')
+                    .val(rowId)
+             );
+          });
+
+
+       });
+    });
+
+    </script>
+    <script type="text/javascript">
+
+    $(document).ready(function() {
        var table = $('#example').DataTable({
           'columnDefs': [
              {
