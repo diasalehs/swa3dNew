3a59eb2c74c722e052ec4d9aa6ca2a2b8c1e2ae4
diasaleh <dia-20111@hotmail.com>
2017-07-25 13:46:58 +0300

    invite

--- resources/views/shared/findVolunteers.blade.php
+++ resources/views/shared/findVolunteers.blade.php
@@ -310,53 +310,7 @@
 
         </form>
         <hr />
-
-        <table id="d" class="display table table-hover  table-bordered"  cellspacing="0"  width="100%">
-          <thead>
-              <tr>
-
-                  <th>Name</th>
-                  <th>Position</th>
-                  <th>Office</th>
-                  <th>Age</th>
-                  <th>Start date</th>
-                  <th>Salary</th>
-              </tr>
-          </thead>
-
-          <tbody>
-              <tr>
-
-                  <td>Tiger Nixon</td>
-                  <td>System Architect</td>
-                  <td>Edinburgh</td>
-                  <td>61</td>
-                  <td>2011/04/25</td>
-                  <td>$320,800</td>
-              </tr>
-              <tr>
-
-                  <td>Garrett Winters</td>
-                  <td>Accountant</td>
-                  <td>Tokyo</td>
-                  <td>63</td>
-                  <td>2011/07/25</td>
-                  <td>$170,750</td>
-              </tr>
-              <tr>
-
-                  <td>Ashton Cox</td>
-                  <td>Junior Technical Author</td>
-                  <td>San Francisco</td>
-                  <td>66</td>
-                  <td>2009/01/12</td>
-                  <td>$86,000</td>
-              </tr>
-
-
-          </tbody>
-      </table>
-
+          @include('includes.FVT')
          <div class="col-sm-12  col-md-8  col-lg-9" style="color: #333">
            <h1 class="greencolor">All Volunteers</h1>
             <table class="table">
