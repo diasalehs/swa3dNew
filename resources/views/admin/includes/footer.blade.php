16f64516ca1253862f8121ed65484386a1a228ff
diasaleh <dia-20111@hotmail.com>
2017-08-02 14:24:04 +0300

    poll an

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -30,7 +30,25 @@
 	    <script src="{{URL::asset('vendor/js/scripts.js')}}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     <script>
-     
+    $(document).ready(function() {
+   var max_fields      = 10; //maximum input boxes allowed
+   var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+   var add_button      = $(".add_field_button"); //Add button ID
+
+   var x = 1; //initlal text box count
+   $(add_button).click(function(e){ //on add input button click
+       e.preventDefault();
+       if(x < max_fields){ //max input box allowed
+        //text box increment
+           $(wrapper).append('<div style="margin:0px;" class="row form-group"><label for="name" class="form-control-label">Answer'+ x +'</label><input type="text"class="form-control col-sm-10 col-md-6 " style=" margin-bottom:6px;"name="answers[]"/><a href="#" class="remove_field col-sm-2 col-md-3">Remove</a></div>'); //add input box
+              x++; 
+       }
+   });
+
+   $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+       e.preventDefault(); $(this).parent('div').remove(); x--;
+   })
+});
     </script>
 </body>
 
