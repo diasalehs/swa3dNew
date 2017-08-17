16d05f2b627cd250b08d92b1b31a228d56f48480
unknown <khaderlaith@gmail.com>
2017-08-17 17:21:26 +0300

    resize

--- app/Http/Controllers/newsController.php
+++ app/Http/Controllers/newsController.php
@@ -22,7 +22,7 @@ class newsController extends Controller
         if ($request->hasFile('mainImg')){
             $mainImg=$request->file('mainImg');
             $imagename=time().'.'.$mainImg->getClientOriginalExtension();
-            Image::make($mainImg)->resize(350,200)->save(public_path('uploads/'.$imagename));
+            Image::make($mainImg)->save(public_path('uploads/'.$imagename));
             $anew->mainImgpath = $imagename;
         }
         $anew->approved=1;
